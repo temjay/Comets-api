6d241b9fe700a2f3e0906a33432358dd6a0b12a5
temjay <temjayofficialmj@gmail.com>
2026-08-20 15:24:11 +0200

    simple " comets/ " routing methods in controller

--- src/comets/comets.controller.ts
+++ src/comets/comets.controller.ts
@@ -1,4 +1,51 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/common';
 
 @Controller('comets')
-export class CometsController {}
+export class CometsController {
+    // Get /comets
+    @Get()
+    getComets() {
+        return {}
+    }
+
+    //Get /comets?type=fast
+    @Get()
+    getFastComets(@Query('type') type: string) {
+        return {
+            type,
+        }
+    }
+
+    // Get /comets/:id
+    @Get(':id')
+    getOneComet(@Param('id') id: string) {
+        return{
+            id,
+        }
+    }
+
+    // Post /comets
+    @Post()
+    createComet(@Body() body: any) {
+        return{
+            body,
+        }
+    }
+
+    // Put /comets/:id
+    @Put(':id')
+    updateComet(@Param('id') id: string, @Body() body: any) {
+        return{
+            id,
+            body,
+        }
+    }
+
+    // Delete /comets/:id
+    @Delete(':id')
+    deleteComet(@Param('id') id: string) {
+        return{
+            id,
+        }
+    }
+}
