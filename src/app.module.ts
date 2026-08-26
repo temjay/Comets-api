0878be9d22f85cad6f6bbaa6217905cb10152dc4
temjay <temjayofficialmj@gmail.com>
2026-08-26 15:22:22 +0200

    Created 'users' module using --nest g resource users--

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CometsModule } from './comets/comets.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [CometsModule],
+  imports: [CometsModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
