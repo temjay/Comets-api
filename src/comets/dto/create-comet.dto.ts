926ec402336a70c50cae13ba23b9408e8c0a8126
temjay <temjayofficialmj@gmail.com>
2026-09-12 11:33:32 +0200

    basic validation on dto/s

--- src/comets/dto/create-comet.dto.ts
+++ src/comets/dto/create-comet.dto.ts
@@ -1,9 +1,11 @@
-import { IsString, MinLength } from "class-validator";
+import { IS_NOT_EMPTY, IsString, MinLength } from "class-validator";
 
 export class CreateCometDto {
     @IsString()
     @MinLength(3, { message: 'Name must at least, be 3 chars long' })
     name: string;
+
     @IsString()
-    type: string;
+    @IS_NOT_EMPTY({ message: 'Speed is required' })
+    speed: string;
 }
--- src/comets/dto/update-comet.dto.ts
+++ src/comets/dto/update-comet.dto.ts
@@ -1,3 +1,7 @@
+import { IsString, MinLength } from "class-validator";
+
 export class UpdateCometDto {
+    @IsString()
+    @MinLength(3, { message: 'Name must at least, be 3 chars long' })
     name: string;
 }
