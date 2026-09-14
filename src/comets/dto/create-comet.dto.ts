9491863639d758b486ba28adfb7b4d3a7f0e786b
temjay <temjayofficialmj@gmail.com>
2026-09-14 12:48:23 +0200

    fixed the dto

--- src/comets/dto/create-comet.dto.ts
+++ src/comets/dto/create-comet.dto.ts
@@ -3,8 +3,8 @@ import { IS_NOT_EMPTY, IsString, MinLength } from "class-validator";
 export class CreateCometDto {
     @IsString()
     @MinLength(3, { message: 'Name must at least, be 3 chars long' })
-    name: string;
+    name!: string;
 
     @IsString()
-    speed: string;
+    speed!: string;
 }
