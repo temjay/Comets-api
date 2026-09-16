f667ce63e92fafcc15896cb5e4b5abfce40a37fb
temjay <temjayofficialmj@gmail.com>
2026-09-16 11:08:28 +0200

    installed auth module to handle authentication

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
 import { CometsModule } from './comets/comets.module.js';
 import { UsersModule } from './users/users.module.js';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [CometsModule, UsersModule],
+  imports: [CometsModule, UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
