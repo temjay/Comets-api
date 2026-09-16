1cc3ef16a9407d2fd1279aa614b77caf3cfcb3fc
temjay <temjayofficialmj@gmail.com>
2026-09-16 11:14:21 +0200

    imported UsersService

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AuthController } from './auth.controller';
-import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller.js';
+import { AuthService } from './auth.service.js';
+import { UsersService } from '../users/users.service.js';
 
 @Module({
+  imports: [UsersService],
   controllers: [AuthController],
   providers: [AuthService]
 })
