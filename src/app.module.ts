c95121235662f4879822a7cff6573065ba8e3670
temjay <temjayofficialmj@gmail.com>
2026-09-25 15:38:40 +0200

    import configmodule and jwt module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,30 @@ import { AppService } from './app.service.js';
 import { CometsModule } from './comets/comets.module.js';
 import { UsersModule } from './users/users.module.js';
 import { AuthModule } from './auth/auth.module.js';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import config from './config/config.js';
 
 @Module({
-  imports: [CometsModule, UsersModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      cache: true,
+      load: [config]
+    }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      global: true,
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+      }),
+
+      inject: [ConfigService],
+    }),
+    CometsModule, 
+    UsersModule, 
+    AuthModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
