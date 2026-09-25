a6c5749fe6e5d26b0488e143798b323db77313c3
temjay <temjayofficialmj@gmail.com>
2026-09-25 17:16:54 +0200

    advanced authenticated user signup - using auth module - hashed passwords - generating jwt tokens

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from '../auth/dto/create-user.dto.js';
+import { UsersService } from '../users/users.service.js';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private readonly usersService: UsersService) { }
+
+    // SignUp endpoint to create a new user
+    @Post('/register')
+    async create(@Body() user: CreateUserDto) {
+        return this.usersService.create(user);
+    }
+
+    // SignIn endpoint to authenticate a user
+    @Post('/login')
+    async login(@Body() credentials: CreateUserDto) {
+        return this.usersService.login(credentials);
+    }
+
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,17 +1,68 @@
-import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto.js';
+import { BadRequestException, Injectable, UnauthorizedException } from '@nestjs/common';
+import { CreateUserDto } from '../auth/dto/create-user.dto.js';
 import { UpdateUserDto } from './dto/update-user.dto.js';
 import { User } from './userInterface.js';
 import { db } from '../prisma/db.js'
+import bcrypt from 'bcryptjs';
+import { JwtService } from '@nestjs/jwt';
 
 
 @Injectable()
 export class UsersService {
+  constructor(private jwtService: JwtService) {}
 
   async create(user: CreateUserDto) {
+    // check if email already exists
+    const existingUser = await db.orm.public.User.where({ email: user.email }).first(); 
+
+    // if user already exists, throw an error
+    if (existingUser) {
+      throw new BadRequestException(`User with email already exists`);
+    }
+
+    //hash the password before saving to the database
+    const hashedPassword = await bcrypt.hash(user.password, 10);
+
+    //create user document and save to the database
     return await db.orm.public.User.create({
       ...user,
-    })
+      password: hashedPassword,
+    });
+
+    // return await db.orm.public.User.create({
+    //   ...user,
+    // })
+  }
+
+  //login method
+  async login(credentials: CreateUserDto) {
+    //find the user by email
+    const user = await db.orm.public.User.where({ email: credentials.email }).first();
+
+    //if user not found, throw an error
+    if (!user) {
+      throw new UnauthorizedException(`Invalid credentials`);
+    }
+    
+    //compare the provided password with the hashed password in the database
+    const isPasswordValid = await bcrypt.compare(credentials.password, user.password);
+
+    //if password is invalid, throw an error
+    if (!isPasswordValid) {
+      throw new UnauthorizedException(`Invalid credentials`);
+    }
+
+    //generate jwt token
+    return this.generateToken(user);
+  }
+
+  //generate token method
+  async generateToken(user) {
+    const accesstoken = this.jwtService.sign({ id: user.id }, {expiresIn: '1h'});
+
+    return {
+      accesstoken,
+    }
   }
 
   async findAll() {
