3785127d2492d03deba6f230b9d133d1f08e5f06
temjay <temjayofficialmj@gmail.com>
2026-09-25 15:40:37 +0200

    removed create-user.dto import

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,5 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { UsersService } from './users.service.js';
-import { CreateUserDto } from './dto/create-user.dto.js';
 import { UpdateUserDto } from './dto/update-user.dto.js';
 
 @Controller('users')
